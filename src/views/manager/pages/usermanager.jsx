ca3afbf16e9f201230d30cdaa5175611b225a5c5
Jackob32 <Jakub.trhlik@gmail.com>
2018-11-17 02:41:34 +0100

    space

--- src/views/manager/pages/usermanager.jsx
+++ src/views/manager/pages/usermanager.jsx
@@ -87,7 +87,7 @@ class Usermanager extends React.Component {
         return (
 
             <div>
-
+<br/>
                 <Button variant="contained" onClick={this.handleClickOpen} color="primary">
                     Nový uživatel
                 </Button>
